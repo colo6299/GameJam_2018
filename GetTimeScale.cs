0c41688f3de5ea6bca808b6aefde13cd611a9a16
Kkajr <37648386+Kkajr@users.noreply.github.com>
2018-09-28 18:21:09 -0700

    Shader stuff

---
+++ GetTimeScale.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetTimeScale : MonoBehaviour {
+    Renderer objRenderer;
+
+    void Start () {
+        objRenderer = GetComponent<Renderer>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        objRenderer.material.SetFloat("_InvertAmount", FlowObject.falseTimescale);
+        print(FlowObject.falseTimescale);
+    }
+
+
+
+}
