df0384090e3231823373b72eb2ae4dc9f3f71f78
Kkajr <37648386+Kkajr@users.noreply.github.com>
2018-09-28 23:11:10 -0700

    Made a basic menu

---
+++ MainMenu.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenu : MonoBehaviour
+{
+    public CanvasRenderer rend;
+    private bool play_clicked;
+    private bool quit_clicked;
+    float i = -1;
+
+
+    public void PlayGame()
+    {
+        SceneManager.LoadSceneAsync(1);
+    }
+
+    public void QuitGame()
+    {
+        print("quit");
+        Application.Quit();
+    }
+
+    private void Update()
+    {  
+        if (play_clicked || quit_clicked)
+        {
+            if (i < 1f)
+                {
+                    UpdatePixels(i);
+                    i += 0.5f * Time.deltaTime;
+                }
+            else if (play_clicked)
+            {
+                i = -1;
+                UpdatePixels(i);
+                play_clicked = false;
+                PlayGame();
+            }
+            else
+            {
+                i = -1;
+                UpdatePixels(i);
+                quit_clicked = false;
+                QuitGame();
+            }
+        }
+        
+    }
+
+    public void PlayButtonClicked()
+    {
+        play_clicked = true;
+    }
+
+    public void QuitButtonClicked()
+    {
+        quit_clicked = true;
+    }
+
+    void UpdatePixels(float i)
+    {
+        print(i);
+        rend.GetMaterial().SetFloat("_InvertAmount", i);
+    }
+
+}
+
+
+
