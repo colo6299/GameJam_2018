b7fc0e5955af56e04b8ee03139d7093babc63449
colo6299 <rocketblox@gmail.com>
2018-09-30 01:27:42 -0700

    njcgd

---
+++ AnimationSpeedController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationSpeedController : MonoBehaviour {
+
+    private Animator anim;
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        anim.speed = FlowObject.falseTimescale;
+    }
+}
