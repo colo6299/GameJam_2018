738c85e4d040e4098eb8b4f65afa3a176944f38d
colo6299 <rocketblox@gmail.com>
2018-09-30 08:30:00 -0700

    bdfhng

---
+++ Audio/AudioFlow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioFlow : MonoBehaviour {
+
+    private AudioSource audioSource;
+
+    void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.PlayOneShot(audioSource.clip);
+    }
+
+    void Update()
+    {
+        audioSource.pitch = FlowObject.falseTimescale;
+    }
+}
