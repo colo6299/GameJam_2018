2242593cbf094a6eec93662f5cc5500c7907902b
colo6299 <rocketblox@gmail.com>
2018-09-30 03:39:18 -0700

    das

---
+++ EnemyDissolve.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDissolve : MonoBehaviour {
+
+    GameObject particleEffect;
+    
+    void OnTriggerEnter(Collider other)
+    {
+        Destroy(Instantiate(particleEffect, other.transform.position, other.transform.rotation, null), 2f);
+        Destroy(other.gameObject);
+    }
+
+
+
+
+
+
+
+
+}
